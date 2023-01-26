7ff506cabd16d960d84ef57a8ee730d11e0770ee
hagoromo2000 <revtm000126@gmail.com>
2023-01-26 15:45:18 +0900

    ポケモン検索機能を追加しました

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,13 @@
 class PokemonsController < ApplicationController
   def new
+    return if params[:search].blank?
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
+ 
+    # JSON.parseをすることで、json形式の文字列をRubyオブジェクトに変換することができる。
+    if raw_response.status == 200
+      @response = JSON.parse(raw_response.body)
+    else #リクエストが失敗した場合の処理
+      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー"
+    end
   end
 end
