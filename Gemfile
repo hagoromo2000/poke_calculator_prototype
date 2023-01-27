aa40e251fc951764ec4956dd819f8a27dc88bfa9
hagoromo2000 <revtm000126@gmail.com>
2023-01-27 15:46:42 +0900

    管理画面の作成 #1

--- Gemfile
+++ Gemfile
@@ -64,4 +64,11 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# HTTPリクエストを送信しAPIを叩く
 gem 'faraday'
+
+#　Bootstrapとfont-awesomeをインストール
+gem 'bootstrap', '~> 4.3.1'
+gem 'font-awesome-sass', '~> 5.11.2'
+
+gem 'jquery-rails'
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,7 @@
 class PokemonsController < ApplicationController
   def new
     return if params[:search].blank?
-    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}" # 基本情報取得
  
     # JSON.parseをすることで、json形式の文字列をRubyオブジェクトに変換することができる。
     if raw_response.status == 200
@@ -9,5 +9,14 @@ class PokemonsController < ApplicationController
     else #リクエストが失敗した場合の処理
       redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー"
     end
+
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon-species/#{params[:search]}" # 日本語名の取得
+
+    if raw_response.status == 200
+      @name_response = JSON.parse(raw_response.body)
+    else #リクエストが失敗した場合の処理
+      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー"
+    end
+
   end
 end
