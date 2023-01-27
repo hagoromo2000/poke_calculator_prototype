8154bb1010317dfba4388ed3089eb78149d58631
hagoromo2000 <revtm000126@gmail.com>
2023-01-27 19:15:08 +0900

    redirectが二重に記載されている問題を修正

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -14,8 +14,6 @@ class PokemonsController < ApplicationController
 
     if raw_response.status == 200
       @name_response = JSON.parse(raw_response.body)
-    else #リクエストが失敗した場合の処理
-      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー"
     end
 
   end
