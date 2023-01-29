d92d1a6c4b9bf5875d610ba83c93e6fd77016b84
hagoromo2000 <revtm000126@gmail.com>
2023-01-29 15:06:04 +0900

    seed.rbにJSONからDBにポケモンデータを取り込む処理を追記しました #3

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,28 +1,3 @@
 class Pokemon < ApplicationRecord
 
-  def get_pokemon_data
-    
-    i = 1
-    while i <= 1008
-      raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{i}" # 基本情報取得
-      name_response = Faraday.get "https://pokeapi.co/api/v2/pokemon-species/#{i}" # 日本語名の取得
-      
-      name = @name_response["names"][0]["name"]
-
-      type1 = @response["types"][0]["type"]["name"]
-      type2 = @response["types"][1]["type"]["name"] if @response["types"][1] 
-
-      ability1 = @response["abilities"][0]["ability"]["name"]
-      ability2 = @response["abilities"][1]["ability"]["name"] if @response["abilities"][1]
-      ability3 = @response["abilities"][2]["ability"]["name"] if @response["abilities"][2]
-
-      hp = @response["stats"][0]["base_stat"]
-      attack = @response["stats"][1]["base_stat"]
-      defense = @response["stats"][2]["base_stat"]
-      special_attack = @response["stats"][3]["base_stat"]
-      special_defense = @response["stats"][4]["base_stat"]
-      speed = @response["stats"][5]["base_stat"]
-    end
-
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(1008) do |i| # ポケモンの初期データ作成
+  file = File.open("json/pokemon_json_data/pokemon#{i}.json", "r") # ポケモンの基本情報を取得
+  json_data = file.read
+  file.close
+  data = JSON.parse(json_data) # jsonデータをハッシュに変換
+
+  file = File.open("json/pokemon_species_json_data/pokemon-species#{i}.json", "r") # ポケモンの日本語名のデータを取得
+  json_data = file.read
+  file.close
+  name_data = JSON.parse(json_data) # jsonデータをハッシュに変換
+
+  name = name_data["names"][0]["name"]
+
+  type1 = data["types"][0]["type"]["name"]
+  type2 = data["types"][1]["type"]["name"] if data["types"][1] 
+
+  ability1 = data["abilities"][0]["ability"]["name"]
+  ability2 = data["abilities"][1]["ability"]["name"] if data["abilities"][1]
+  ability3 = data["abilities"][2]["ability"]["name"] if data["abilities"][2]
+
+  hp = data["stats"][0]["base_stat"]
+  attack = data["stats"][1]["base_stat"]
+  defense = data["stats"][2]["base_stat"]
+  special_attack = data["stats"][3]["base_stat"]
+  special_defense = data["stats"][4]["base_stat"]
+  speed = data["stats"][5]["base_stat"]
+
+  Pokemon.create!(
+    name: name,
+    type1: type1,
+    type2: type2,
+    ability1: ability1,
+    ability2: ability2,
+    ability3: ability3,
+    hp: hp,
+    attack: attack,
+    defense: defense,
+    special_attack: special_attack,
+    special_defense: special_defense,
+    speed: speed
+  )
+
+end
