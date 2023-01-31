1a131e3eaa871b739c6a9e15a020827578b64299
hagoromo2000 <revtm000126@gmail.com>
2023-01-31 13:05:25 +0900

    Postモデルのresourcesルーティングを追加 #6

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pokemons#new'
   resources :pokemons
+  resources :posts
   namespace :admin do
     root to: 'dashboards#index'
   end
